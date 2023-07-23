e742fb86c03aca9b59112540f8c4a391b5ca6f72
HakanKara75 <hakanbatirhan@gmail.com>
2023-07-23 21:46:52 +0300

    v

--- src/test/java/Appium/Appium01.java
+++ src/test/java/Appium/Appium01.java
@@ -1,4 +1,31 @@
 package Appium;
 
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Appium01 {
+    @Test
+    public void test() throws MalformedURLException {
+
+        DesiredCapabilities capabilities= new DesiredCapabilities();
+//        capabilities.setCapability("platformName", "Android");
+//        capabilities.setCapability(CapabilityType.PLATFORM_NAME, "Android");
+        /*
+        yukaridaki 2 kod da asagidaki kodun gorevini yapar. Herhangi birini yazsak olur.
+         */
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0"); //13 kismi kullanilacak telefondaki Android versionudur.
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
+//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice"); IOS'da RealDevice yerine cmd de "adb device" yazinca cikan kodu girecegiz
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+    }
 }
