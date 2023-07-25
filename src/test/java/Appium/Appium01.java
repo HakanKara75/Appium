dc92e81ae3a36bb623df774a3169abf373add57c
HakanKara75 <hakanbatirhan@gmail.com>
2023-07-25 20:56:34 +0300

    v

--- src/test/java/Appium/Appium01.java
+++ src/test/java/Appium/Appium01.java
@@ -20,10 +20,11 @@ public class Appium01 {
         yukaridaki 2 kod da asagidaki kodun gorevini yapar. Herhangi birini yazsak olur.
          */
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13.0"); //13 kismi kullanilacak telefondaki Android versionudur.
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10"); //13 kismi kullanilacak telefondaki Android versionudur.
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
 //        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice"); IOS'da RealDevice yerine cmd de "adb device" yazinca cikan kodu girecegiz
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, "/Users/HakanBatirhan/IdeaProjects/Appium_Lesson/src/Apps/Gesture Tool_1.3_Apkpure.apk");
 
         AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
 
