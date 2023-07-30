270ed6b1d394e09c8523e2a03bff7be036587c42
HakanKara75 <hakanbatirhan@gmail.com>
2023-07-30 13:53:30 +0300

    v

--- src/test/java/baseClass/BaseClass.java
+++ src/test/java/baseClass/BaseClass.java
@@ -1,42 +1,42 @@
-package Appium;
+package baseClass;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
-import utilities.ReusableMethods;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import static org.testng.AssertJUnit.assertEquals;
-
-public class Appium08 {
+public class BaseClass {
     @Test
-    public void test() throws MalformedURLException {
+    public AndroidDriver getAndroidDriver_Emulator() throws MalformedURLException {
+
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10"); //13 kismi kullanilacak telefondaki Android versionudur.
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
-//        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice"); IOS'da RealDevice yerine cmd de "adb device" yazinca cikan kodu girecegiz
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         capabilities.setCapability(MobileCapabilityType.APP, "/Users/HakanBatirhan/IdeaProjects/Appium_Lesson/src/Apps/Gesture_Tool_1.3_Apkpure.apk");
-//        capabilities.setCapability("appPackage", "com.davemac327.gesture.tool");
-//        capabilities.setCapability("appActivity", "com.davemac327.gesture.tool.GestureBuilderActivit");
         capabilities.setCapability("noReset", true);
         AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        return driver;
+    }
 
-        WebElement resourceId=driver.findElementByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/addButton\")");
-        resourceId.click();
+    @Test
+    public AndroidDriver getAndroidDriver_RealDevice() throws MalformedURLException {
 
-        ReusableMethods.bekle(3);
-        String  doneButton=driver.findElementByAndroidUIAutomator("UiSelector().resourceId(\"com.davemac327.gesture.tool:id/done\").enabled(false)").getAttribute("enabled").toString();
 
-        assertEquals(doneButton, "false");
-
-        driver.closeApp();
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13"); //13 kismi kullanilacak telefondaki Android versionudur.
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "RealDevice");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, "/Users/HakanBatirhan/IdeaProjects/Appium_Lesson/src/Apps/Gesture_Tool_1.3_Apkpure.apk");
+        capabilities.setCapability("noReset", true);
+        AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        return driver;
     }
 }
