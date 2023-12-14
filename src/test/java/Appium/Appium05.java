00b1c9b3ebfcb405acd2fdfcf6465cedcb69e603
hakankara75 <hakanbatirhan@gmail.com>
2023-12-14 14:21:15 +0300

    v

--- src/test/java/Appium/Appium05.java
+++ src/test/java/Appium/Appium05.java
@@ -7,8 +7,6 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
